ebda621d5d8e37df7895b06b7648ad1165630884
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-21 12:43:46 +0530

    Add color palettes in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,21 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					100: '#b9dff9',
+					200: '#649ccc',
+					300: '#4d72a6'
+				},
+				secondary: {
+					100: '#f7f8fb',
+					200: '#9ba2aa',
+					300: '#525862'
+				},
+				dark: '#0d0d0d'
+			}
+		}
 	},
 
 	plugins: []
