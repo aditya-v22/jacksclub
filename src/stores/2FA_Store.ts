95ad161b7c8ed6db9a58fc6d83a02a14e779cd9e
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-21 21:57:04 +0530

    Add store for 2FA

---
+++ src/stores/2FA_Store.ts
@@ -0,0 +1,37 @@
+import { writable } from 'svelte/store';
+
+interface CodeVerificationState {
+	code: string[];
+	activeFieldIndex: number;
+	isCodeValid: boolean;
+	remainingDigits: number;
+}
+
+const defaultState: CodeVerificationState = {
+	code: Array(6).fill(''), // 6 empty code fields
+	activeFieldIndex: 0,
+	isCodeValid: false,
+	remainingDigits: 6
+};
+
+export const TOTAL_DIGITS = 6;
+export const twoFA_Store = writable<CodeVerificationState>(defaultState);
+
+export const setCode = (value: string, index: number) => {
+	twoFA_Store.update((state) => {
+		const code = [...state.code.slice(0, index), value, ...state.code.slice(index + 1)];
+		const isCodeValid = code.join('') === '123456';
+		const remainingDigits = code.filter((digit) => digit === '').length;
+
+		return {
+			...state,
+			code,
+			isCodeValid,
+			remainingDigits
+		};
+	});
+};
+
+export const setActiveFieldIndex = (index: number) => {
+	twoFA_Store.update((state) => ({ ...state, activeFieldIndex: index }));
+};
