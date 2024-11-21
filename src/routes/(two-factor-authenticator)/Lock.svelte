91709e348f6811a24f6b02c26318c2f145887119
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-21 21:56:17 +0530

    Add lock component for 2FA

---
+++ src/routes/(two-factor-authenticator)/Lock.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Lock, Unlock } from 'lucide-svelte';
+	import { twoFA_Store } from '../../stores/2FA_Store';
+	import { cn } from '$lib/utils/helper';
+
+	const isValidCode = $derived($twoFA_Store.isCodeValid);
+	const isInvalidCode = $derived(!$twoFA_Store.isCodeValid && $twoFA_Store.remainingDigits === 0);
+</script>
+
+<div class="flex w-full justify-center">
+	<div
+		class={cn(
+			'bg-primary-100/40 relative flex h-28 w-28 items-center justify-center rounded-full transition-all duration-1000',
+			{
+				'bg-green-100': isValidCode,
+				'animate-scale-and-reset bg-red-100': isInvalidCode
+			}
+		)}
+	>
+		<div
+			class={cn('absolute inset-0 z-0 h-full w-full rounded-full bg-transparent transition-all', {
+				'animate-ping-once-success': isValidCode,
+				'animate-ping-once-error': isInvalidCode
+			})}
+		></div>
+
+		<span class="z-10">
+			{#if isValidCode}
+				<Unlock size={40} class="text-green-500" />
+			{:else}
+				<Lock size={40} class={cn('text-sky-500', { 'text-red-500': isInvalidCode })} />
+			{/if}
+		</span>
+	</div>
+</div>
