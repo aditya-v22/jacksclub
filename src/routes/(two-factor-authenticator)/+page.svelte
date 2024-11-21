575bacf9ec508e2f5ca9ed89d5863c29fe880f07
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-21 21:57:59 +0530

    Aggregate all the 2FA components and store and show in UI

---
+++ src/routes/(two-factor-authenticator)/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import CodeForm from './CodeForm.svelte';
+	import DigitCounter from './DigitCounter.svelte';
+	import Lock from './Lock.svelte';
+</script>
+
+<div class="min-w-screen flex min-h-screen items-center justify-center bg-transparent">
+	<div class="box-s shadow-card m-8 min-w-[550px] rounded-3xl bg-white p-12">
+		<Lock />
+
+		<div class="mb-14 mt-4 text-center">
+			<h1 class="text-dark text-5xl font-semibold">Easy peasy</h1>
+			<p class="text-secondary-200 mt-6 font-medium">
+				Enter 6-digit code from your two factor authenticator APP.
+			</p>
+		</div>
+
+		<CodeForm />
+		<DigitCounter />
+	</div>
+</div>
