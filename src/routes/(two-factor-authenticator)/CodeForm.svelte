aca55857f6e8190c0dfe923befaf75744e6d3a52
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-26 18:38:07 +0530

    Refactor code form component

--- src/routes/(two-factor-authenticator)/CodeForm.svelte
+++ src/routes/(two-factor-authenticator)/CodeForm.svelte
@@ -1,62 +1,22 @@
 <script lang="ts">
 	import { cn } from '$lib';
 	import { useOutsideClick } from '$lib/actions/useOutsideClick';
-	import { validateNumber } from '$lib/validations';
 	import DigitInput from '../../modules/auth/components/DigitInput.svelte';
-	import { TOTAL_DIGITS } from '../../modules/auth/constants/2FA';
-	import { twoFA_Store, setActiveFieldIndex, setCode } from '../../modules/auth/stores/2FA_Store';
-
-	const secondLastIndex = TOTAL_DIGITS - 1;
+	import { twoFA_Store, setActiveFieldIndex } from '../../modules/auth/stores/2FA_Store';
+	import { moveFocus } from '../../modules/auth/utils/2FA';
+	import {
+		handleCodePaste,
+		handleInputChange,
+		handleInputClick,
+		handleKeydown
+	} from './codeInputHandlers';
 
 	const code = $derived($twoFA_Store.code);
 	const activeFieldIndex = $derived($twoFA_Store.activeFieldIndex);
 	const isInvalidCode = $derived($twoFA_Store.remainingDigits === 0 && !$twoFA_Store.isCodeValid);
 
-	// Move focus to next input
-	const moveFocus = (index: number) => {
-		const inputs = document.querySelectorAll<HTMLInputElement>('#code-form input');
-
-		if (inputs[index]) {
-			inputs[index].focus();
-			setActiveFieldIndex(index);
-		}
-	};
-
-	// Handle backspace (move focus to previous field)
-	const handleBackspace = (event: KeyboardEvent, index: number) => {
-		if (event.key === 'Backspace' && index > 0) {
-			const currentIndex =
-				index === secondLastIndex && code[secondLastIndex] !== '' ? secondLastIndex : index - 1;
-
-			setCode('', index);
-			moveFocus(currentIndex);
-		}
-	};
-
-	// Handle input change
-	const handleInputChange = (event: Event, index: number) => {
-		const input = event.target as HTMLInputElement;
-		const value = input.value;
-		const hasValue = value.length === 1;
-
-		if (!validateNumber(value)) {
-			// Clear the current input if it's not a digit
-			setCode('', index);
-			return;
-		}
-
-		setCode(value, index);
-
-		if (hasValue && index < secondLastIndex) {
-			moveFocus(index + 1);
-		} else if (hasValue && index === secondLastIndex) {
-			setActiveFieldIndex(-1);
-		}
-	};
-
 	$effect(() => {
-		window.alert(`Hello!
-Please use the correct code "123456" to see the success UI.`);
+		window.alert(`Hello!\nPlease use the correct code "123456" to see the success UI.`);
 
 		moveFocus(0);
 	});
@@ -74,9 +34,10 @@ Please use the correct code "123456" to see the success UI.`);
 			invalid={isInvalidCode}
 			focused={index === activeFieldIndex}
 			bind:value={$twoFA_Store.code[index]}
-			onClick={() => moveFocus(index)}
-			onInputChange={(event) => handleInputChange(event, index)}
-			onKeyDown={(event) => handleBackspace(event, index)}
+			onClick={() => handleInputClick(code, index)}
+			onInputChange={(event) => handleInputChange(event, code, index)}
+			onKeyDown={(event) => handleKeydown(event, code, index)}
+			onPaste={handleCodePaste}
 		/>
 
 		{#if index === 2}
