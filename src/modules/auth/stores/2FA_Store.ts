c0f9cefc64a910405ac24407640da1cd67427e03
Aditya Vaishnav <aditya.vaishanv2022@gmail.com>
2024-11-26 17:19:29 +0530

    Update 2FA store

--- src/modules/auth/stores/2FA_Store.ts
+++ src/modules/auth/stores/2FA_Store.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store';
+import { calculateRemainingDigits, validate2FA_Code } from '../utils/2FA_Helpers';
 
 interface CodeVerificationState {
 	code: string[];
@@ -18,19 +19,28 @@ export const twoFA_Store = writable<CodeVerificationState>(defaultState);
 
 export const setCode = (value: string, index: number) => {
 	twoFA_Store.update((state) => {
-		const code = [...state.code.slice(0, index), value, ...state.code.slice(index + 1)];
-		const isCodeValid = code.join('') === '123456';
-		const remainingDigits = code.filter((digit) => digit === '').length;
+		const newCode = [...state.code];
+		newCode[index] = value;
 
 		return {
 			...state,
-			code,
-			isCodeValid,
-			remainingDigits
+			code: newCode,
+			isCodeValid: validate2FA_Code(newCode),
+			remainingDigits: calculateRemainingDigits(newCode)
 		};
 	});
 };
 
 export const setActiveFieldIndex = (index: number) => {
 	twoFA_Store.update((state) => ({ ...state, activeFieldIndex: index }));
+};
+
+export const clearCode = () => {
+	twoFA_Store.update((state) => ({
+		...state,
+		code: Array(6).fill(''),
+		activeFieldIndex: 0,
+		isCodeValid: false,
+		remainingDigits: 6
+	}));
 };
